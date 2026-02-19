01cfd5bd2a789e5b146b4329472682bde529ad2c
Wesleysc94 <Wesley.sc83@gmail.com>
2026-02-18 22:15:08 -0300

    Update print statements in autotest function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Passou')
+        print('Passou 1')
     else:
-        print('Falhou')
+        print('Falhou 2')
 
 in_autotests_we_trust(10, '10')
 
